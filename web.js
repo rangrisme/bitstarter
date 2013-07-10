0547748e779e4c8fa24669be8868d76d28f3103d
Ubuntu <ubuntu@ip-172-31-23-47.us-west-2.compute.internal>
2013-07-10 17:01:01 +0000

    Change web.js according code from fellow coursera

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  var fs=require('fs');
- var buffer = new buffer();
-  response.send(buffer.toString('utc 8',fs.readFileSync("index.html"));
+ var buffer = new Buffer(fs.readFileSync('index.html'));
+  response.send(buffer.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
